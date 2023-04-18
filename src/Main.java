957a81bb28afe759baa1bdbfc7f6073a3ff964b4
Victor <vgarher193@g.educaand.es>
2023-04-18 17:57:42 +0200

    codigo modificado en rama CR0001

--- src/Main.java
+++ src/Main.java
@@ -79,29 +79,5 @@ public class Main {
         } while (opcion != 5);
     }
 
-    static class Producto {
-        private String nombre;
-        private double precio;
 
-        public Producto(String nombre, double precio) {
-            this.nombre = nombre;
-            this.precio = precio;
-        }
-
-        public String getNombre() {
-            return nombre;
-        }
-
-        public void setNombre(String nombre) {
-            this.nombre = nombre;
-        }
-
-        public double getPrecio() {
-            return precio;
-        }
-
-        public void setPrecio(double precio) {
-            this.precio = precio;
-        }
-    }
 }
